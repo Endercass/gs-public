f8c83d9488fe358815aed135e88f8ba63bdaaa65
endercass <46408156+Endercass@users.noreply.github.com>
2024-07-30 20:36:09 -0400

    cors

--- src/api/service.rs
+++ src/api/service.rs
@@ -7,15 +7,23 @@ use axum::{
     routing::{get, post},
     Router,
 };
+use hyper::Method;
+use tower_http::cors::{Any, CorsLayer};
 
 use crate::APIState;
 
 use super::encode_url::post_encode;
 
 pub fn service(state: Arc<APIState>) -> Router {
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST, Method::OPTIONS])
+        .allow_headers(Any)
+        .allow_origin(Any);
+
     Router::new()
         .route("/", get(index))
         .route("/encode", post(post_encode))
+        .layer(cors)
         .with_state(state)
 }
 
